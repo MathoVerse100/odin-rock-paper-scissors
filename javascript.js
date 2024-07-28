0532721eb5cda712181889626e4601eb43190c11
MathoVerse100 <mathoverse100@gmail.com>
2024-07-28 18:22:21 +0300

    Created the basic UI and the createGameBoard function which creates the game layout

--- javascript.js
+++ javascript.js
@@ -65,4 +65,80 @@ function getHumanChoice() {
 };
 
 
-document.getElementById("startGame").onclick = playGame;
+function createGameBoard() {    
+    const separator1 = document.createElement("p");
+    const gamePlace = document.createElement("div");
+    const separator2 = document.createElement("p");
+    const endGame = document.createElement("button");
+
+    const roundNumber = document.createElement("h3");
+    const buttons = document.createElement("div");
+    const results = document.createElement("div");
+
+    const rock = document.createElement("button");
+    const paper = document.createElement("button");
+    const scissors = document.createElement("button");
+    const resultBroadcast = document.createElement("h3");
+    const humanScore = document.createElement("p");
+    const computerScore = document.createElement("p");
+
+
+    separator1.classList.add("separator1");
+    gamePlace.classList.add("gamePlace");
+    separator2.classList.add("separator2");
+    endGame.classList.add("endGame");
+
+    roundNumber.classList.add("roundNumber");
+    buttons.classList.add("buttons");
+    results.classList.add("results");
+
+    rock.classList.add("rock");
+    paper.classList.add("paper");
+    scissors.classList.add("scissors");
+    resultBroadcast.classList.add("resultBroadcast");
+    humanScore.classList.add("humanScore");
+    computerScore.classList.add("computerScore");
+
+
+    buttons.style.display = "flex";
+    buttons.style.gap = "8px";
+    resultBroadcast.style.color = "blue";
+
+    
+    separator1.textContent = "-".repeat(90);
+    roundNumber.textContent = "Round ___";
+    rock.textContent = "Rock";
+    paper.textContent = "Paper";
+    scissors.textContent = "Scissors";
+    resultBroadcast.textContent = "No one started yet...";
+    humanScore.textContent = "Your Score: 0";
+    computerScore.textContent = "Computer Score: 0";
+    separator2.textContent = "-".repeat(90);
+    endGame.textContent = "End Game";
+
+    buttons.append(rock, paper, scissors);
+    results.append(resultBroadcast, humanScore, computerScore);
+
+    gamePlace.append(roundNumber, buttons, results);
+
+    document.body.append(separator1, gamePlace, separator2, endGame);
+
+    return {
+        "separator1": separator1,
+        "gamePlace": gamePlace,
+        "separator2": separator2,
+        "endGame": endGame,
+        "roundNumber": roundNumber,
+        "buttons": buttons,
+        "results": results,
+        "rock": rock,
+        "paper": paper,
+        "scissors": scissors,
+        "resultBroadcast": resultBroadcast,
+        "humanScore": humanScore,
+        "computerScore": computerScore
+    };
+};
+
+
+const startGame = document.querySelector(".startGame");
